549aa24977b4d15a27e90975205a73235f1c75e3
core.editor <andreeavillatte@gmail.com>
2019-04-24 15:24:09 +0200

    après corrections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 
-// on declanche le compteur the counter. On l'initialise à 0
+//  on declare une variable count qu'on initialise à 0
 var count=0;
+// on appelle une functione annonime avec la methode Click avec le bouton "button"
 $('button').click(function(){
   // chaque fois qu'on click sur le bouton qui a la classe .button
     count++; // le compteur ajout encore une valeur, encore un 1
